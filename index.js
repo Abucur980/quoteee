b134184711cecdaf582281d626bc20af684a887a
Andrei Bucur <abucur980@gmail.com>
2021-11-12 22:19:27 +0200

    added condition for longer quoteee text

--- index.js
+++ index.js
@@ -11,23 +11,30 @@ function getNewQuote() {
     // pick a random api quote   
     const quoteee = apiQuotes[Math.floor(Math.random() * apiQuotes.length)];
     // check to see if author is unknown
-    if (!quoteee.author){
+    if (!quoteee.author) {
         quoteAuthor.textContent = "Unknown";
-    }else{
+    } else {
         quoteAuthor.textContent = quoteee.author;
+    }
+    
+    // check the length of the quote and then determine the style that should be applied
+    if (quoteee.text.length > 50) {
+        quoteText.classList.add('long-quote');
+    } else {
+        quoteText.classList.remove('long-quote');
     }
     quoteText.textContent = quoteee.text;
 }
 
 // get those quotes from the api
 async function getQuotes() {
-    const apiURL='https://type.fit/api/quotes';
+    const apiURL = 'https://type.fit/api/quotes';
 
-    try{
+    try {
         const response = await fetch(apiURL);
         apiQuotes = await response.json();
         getNewQuote();
-    } catch(error){
+    } catch (error) {
 
     }
 }
