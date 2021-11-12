26a29ddc7b60c48844ad6bceb343d380ac57a98b
Andrei Bucur <abucur980@gmail.com>
2021-11-12 22:14:11 +0200

    add initial variables and quoteee condition if author is missing

--- index.js
+++ index.js
@@ -1,9 +1,22 @@
+const quoteContainer = document.getElementById('quote-container');
+const quoteText = document.getElementById('quote');
+const quoteAuthor = document.getElementById('author');
+const twitterBtn = document.getElementById('twitter');
+const newQuoteBtn = document.getElementById('new-quote');
+
 let apiQuotes = [];
 
 // get a new quote
 function getNewQuote() {
     // pick a random api quote   
     const quoteee = apiQuotes[Math.floor(Math.random() * apiQuotes.length)];
+    // check to see if author is unknown
+    if (!quoteee.author){
+        quoteAuthor.textContent = "Unknown";
+    }else{
+        quoteAuthor.textContent = quoteee.author;
+    }
+    quoteText.textContent = quoteee.text;
 }
 
 // get those quotes from the api
