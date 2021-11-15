fdb29deecbd97e2a87932940efe6c17678402233
Andrei Bucur <abucur980@gmail.com>
2021-11-15 20:22:28 +0200

    add the html css and js for the loading animation

--- index.js
+++ index.js
@@ -3,11 +3,25 @@ const quoteText = document.getElementById('quote');
 const quoteAuthor = document.getElementById('author');
 const twitterBtn = document.getElementById('twitter');
 const newQuoteBtn = document.getElementById('new-quote');
+const loader = document.getElementById('loader');
 
 let apiQuotes = [];
 
+// show loading
+function loading() {
+    loader.hidden = false;
+    quoteContainer.hidden = true;
+}
+
+// Hide loading
+function complete() {
+    quoteContainer.hidden = false;
+    loader.hidden = true;
+}
+
 // get a new quote
 function getNewQuote() {
+    loading();
     // pick a random api quote   
     const quoteee = apiQuotes[Math.floor(Math.random() * apiQuotes.length)];
     // check to see if author is unknown
@@ -23,11 +37,14 @@ function getNewQuote() {
     } else {
         quoteText.classList.remove('long-quote');
     }
+    // set quote and hide loader
     quoteText.textContent = quoteee.text;
+    complete();
 }
 
 // get those quotes from the api
 async function getQuotes() {
+    loading();
     const apiURL = 'https://type.fit/api/quotes';
 
     try {
